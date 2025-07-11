9fa4dc98b286aabd76ce7bee6302f468e457a6a1
cobmg <rankanggong@outlook.com>
2025-07-11 09:30:46 +0800

    Modify model

--- OrderCreationDemo/Models/Product.cs
+++ OrderCreationDemo/Models/Product.cs
@@ -5,11 +5,4 @@ namespace OrderCreationDemo.Models
         public Guid ProductId { get; set; }
         public int Quantity { get; set; }
     }
-
-    public class ProductDTO
-    {
-        public int Id { get; set; }
-        public Guid ProductId { get; set; }
-        public int Quantity { get; set; }
-    }
 }
